79c2d7e5dc5c4d028463c39a45e0241554603e90
Mannat Nandi <mannatnandi755@gmail.com>
2026-03-17 23:15:22 +0530

    FLask backend setup done use username test@gmail.com password 1234 for demo credentials

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     
     SECRET_KEY = "kinsync_secret_key"
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root123@localhost/kinsync_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root1234@localhost/kinsync_db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
